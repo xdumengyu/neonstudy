b8dff106414ed5a4d36b6d41ae4f649484aa1bc2
xdumengyu <xdumengyu@gmail.com>
2018-03-25 14:44:22 +0800

    opencv added, test pass, cv_5.64ms, neon_71.5ms, qwq

--- basic/winograd/build.sh
+++ basic/winograd/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 mkdir -p build
 pushd build
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a  -DANDROID_TOOLCHAIN=gcc ..
+export OpenCV_DIR="/home/mintmy/Downloads/OpenCV-android-sdk/sdk/native/jni"
+export ANDROID_NDK="/home/mintmy/kenzo/android-ndk-r14b"
+
+# def DANDROID_NDK_ABI_NAME for opencv link
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DOpenCV_DIR=${OpenCV_DIR} -DANDROID_ABI="arm64-v8a" -DANDROID_TOOLCHAIN=gcc -DANDROID_NDK_ABI_NAME="arm64-v8a" ..
 make 
 popd
--- basic/winograd/test/test_2x2_3x3.cpp
+++ basic/winograd/test/test_2x2_3x3.cpp
@@ -3,10 +3,14 @@
 #include <stdlib.h>
 #include "func.h"
 #include <vector>
+#include <chrono>
+#include <opencv2/opencv.hpp>
 using namespace std;
+using namespace cv;
+using namespace chrono;
 void constructFakeImg(vector<float> &fake, size_t rows, size_t cols)
 {
-	int nElement = rows * cols;
+	size_t nElement = rows * cols;
 	fake.reserve(nElement);
 	srand(time(NULL));
 	for (size_t i = 0; i < nElement; ++i) 
@@ -15,7 +19,7 @@ void constructFakeImg(vector<float> &fake, size_t rows, size_t cols)
 	}
 	return ;
 }
-int main()
+int fakeTest()
 {
 	const int rows = 1024;
 	const int cols = 768;
@@ -50,5 +54,59 @@ int main()
 		}
 		printf("\n");
 	}
+	return 0;
+}
+int main(int argc, char **argv)
+{
+	Mat srcImage = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	Mat srcFloat;
+	int rows = srcImage.rows;
+	int cols = srcImage.cols;
+	srcImage.convertTo(srcFloat, CV_32FC1);
+	Mat dstFloat(srcImage.rows, srcImage.cols, CV_32FC1);
+	const int ITER = 20;
+	float ker[9] = {
+			1,1,1,
+			1,1,1,
+			1,1,1
+	};
+	//normalize kernel
+	float sum = std::accumulate(ker, ker + 9, 0.0);
+	std::transform(ker, ker + 9, ker, [sum](float x) { return x / sum;});
+	Mat kernel(3,3, CV_32FC1, ker);
+	// cv func
+	auto cv_start = system_clock::now();
+	for (int iter = 0; iter < ITER; ++iter) {
+			filter2D(srcFloat, dstFloat, srcFloat.depth(), kernel);
+	}
+	
+	auto cv_end = system_clock::now();
+	auto dur = duration_cast<milliseconds>(cv_end - cv_start);
+	cout<<"cv version cost:\n";
+	cout<<static_cast<double>(dur.count()) / ITER<<" ms\n";
+	Mat dstImage;
+	dstFloat.convertTo(dstImage, CV_8UC1);
+	imwrite("cv_out.png", dstImage);
+	// my neon 2x2_3x3
+	auto neon_start = system_clock::now();
+	for (int iter = 0; iter < ITER; ++iter) {
+		conv3x3s1_winograd(
+						(float *)(dstFloat.data),
+						cols,
+						(float *)(srcFloat.data),
+						cols,
+						rows,
+						cols,
+						ker
+						);
+		
+	}
+	auto neon_end = system_clock::now();
+	dur = duration_cast<milliseconds>(neon_end - neon_start);
+	cout<<"neon version cost:\n";
+	cout<<static_cast<double>(dur.count()) / ITER<<" ms\n";
+	dstFloat.convertTo(dstImage, CV_8UC1);
+	imwrite("neon_out.png", dstImage);
+	
 	return 0;
 }
